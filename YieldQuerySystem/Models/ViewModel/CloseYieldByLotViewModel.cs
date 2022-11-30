9647ac6579f05e19fefdf88f6ed402e0b56a32f3
fgtyu2001@gmail.com <fgtyu2001@gmail.com>
2022-12-01 03:17:58 +0800

    CloseYieldbyLot add Chart and First Phrase Table

--- YieldQuerySystem/Models/ViewModel/CloseYieldByLotViewModel.cs
+++ YieldQuerySystem/Models/ViewModel/CloseYieldByLotViewModel.cs
@@ -8,7 +8,7 @@ namespace YieldQuerySystem.Models.ViewModel
         public int QtyIssue { get; set; }
         public int QtyOut { get; set; }
         public string OverAllYield { get; set; }
-        public int LossQTY { get; set; }
+        public int LossQty { get; set; }
         public string CloseDT { get; set; }
         public List<LossData> lossDatas { get; set; }
         
